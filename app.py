33233c87ab5883560a83bd28ce74e776683d5e56
Husniddin <uzbekfreedom@gmail.com>
2022-11-29 22:14:49 +0500

    .

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from utils.set_bot_commands import set_default_commands
 async def on_startup(dispatcher):
     # Birlamchi komandalar (/star va /help)
     await set_default_commands(dispatcher)
-    # Bot ishga tushgani haqida adminga xabar berish
    
 if __name__ == '__main__':
     executor.start_polling(dp, on_startup=on_startup)
