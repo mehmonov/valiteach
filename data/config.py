0e71dae133d08c3284b62990a82903728d36068d
Husniddin <uzbekfreedom@gmail.com>
2022-11-29 22:17:27 +0500

    Revert "."
    This reverts commit a9e35cbd4914c6e020c34abc96a9185f5460bee8.

--- data/config.py
+++ data/config.py
@@ -1,10 +1,10 @@
 from environs import Env
-
+import os
 # environs kutubxonasidan foydalanish
 env = Env()
 env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-IP = env.str("ip")  # Xosting ip manzili
+BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))  # Bot toekn
+ADMINS =   str(os.environ.get("ADMINS"))# adminlar ro'yxati
+IP = str(os.environ.get("ip")) # Xosting ip manzili
